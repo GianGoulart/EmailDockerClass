49a24c215fb6752ae34116a47a9c00cce486f89f
Gian Goulart <giangoulart1994@gmail.com>
2020-01-19 22:49:27 -0300

    Implementing redis

--- worker/main.go
+++ worker/main.go
@@ -1,10 +1,11 @@
 package main
 
-import "github.com/go-redis/redis/v7"
+//import "github.com/go-redis/redis/v7"
+import "fmt"
 
 func main(){
 
-	client := redis.NewClient(&redis.Options{
+/*	client := redis.NewClient(&redis.Options{
 		Addr:     "queue:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
@@ -13,5 +14,11 @@ func main(){
 	pong, err := client.Ping().Result()
 	fmt.Println(pong, err)
 
+	result, err := client.BLPop(1*time.Second, "queue").Result()
+	if err != nil {
+		panic(err)
+	}
+*/
+	fmt.Println("Hello")
 	// Output: PONG <nil>
 }
